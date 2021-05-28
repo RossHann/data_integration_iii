bd32156f56783f56ba45dd36498048cb6135a1c4
Russell Han <49119975+RossHann@users.noreply.github.com>
2021-05-28 23:25:35 +0800

    add: unfinished algorithm

--- main.py
+++ main.py
@@ -16,30 +16,32 @@ num_data = np.zeros((100, 2))
 num_data[:, 0] = x
 num_data[:, 1] = y
 
+print(num_data)
+
 # quantify the difference between the two curves using PCM
-pcm = similaritymeasures.pcm(exp_data, num_data)
+# pcm = similaritymeasures.pcm(exp_data, num_data)
 
 # quantify the difference between the two curves using
 # Discrete Frechet distance
-df = similaritymeasures.frechet_dist(exp_data, num_data)
+# df = similaritymeasures.frechet_dist(exp_data, num_data)
 
 # quantify the difference between the two curves using
 # area between two curves
-area = similaritymeasures.area_between_two_curves(exp_data, num_data)
+# area = similaritymeasures.area_between_two_curves(exp_data, num_data)
 
 # quantify the difference between the two curves using
 # Curve Length based similarity measure
-cl = similaritymeasures.curve_length_measure(exp_data, num_data)
+# cl = similaritymeasures.curve_length_measure(exp_data, num_data)
 
 # quantify the difference between the two curves using
 # Dynamic Time Warping distance
-dtw, d = similaritymeasures.dtw(exp_data, num_data)
+# dtw, d = similaritymeasures.dtw(exp_data, num_data)
 
 # print the results
-print(pcm, df, area, cl, dtw)
-
-# plot the data
-plt.figure()
-plt.plot(exp_data[:, 0], exp_data[:, 1])
-plt.plot(num_data[:, 0], num_data[:, 1])
-plt.show()
+# print(pcm, df, area, cl, dtw)
+#
+# # plot the data
+# plt.figure()
+# plt.plot(exp_data[:, 0], exp_data[:, 1])
+# plt.plot(num_data[:, 0], num_data[:, 1])
+# plt.show()
